48c511e31f6b9bd9f6bd3ca477023ab4c966735f
burravenky99 <burravenky1899@gmail.com>
2025-11-10 10:42:15 +0530

    Add files via upload

---
+++ exp3.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+typedef struct {
+    int pid;
+    int burstTime;
+    int waitingTime;
+    int turnAroundTime;
+} Process;
+
+void findWaitingTime(Process proc[], int n) {
+    proc[0].waitingTime = 0;  
+    for (int i = 1; i < n; i++) {
+        proc[i].waitingTime = proc[i - 1].waitingTime + proc[i - 1].burstTime;
+    }
+}
+
+void findTurnAroundTime(Process proc[], int n) {
+    for (int i = 0; i < n; i++) {
+        proc[i].turnAroundTime = proc[i].burstTime + proc[i].waitingTime;
+    }
+}
+
+void findAvgTime(Process proc[], int n) {
+    int totalWaitingTime = 0, totalTurnAroundTime = 0;
+    for (int i = 0; i < n; i++) {
+        totalWaitingTime += proc[i].waitingTime;
+        totalTurnAroundTime += proc[i].turnAroundTime;
+    }
+    printf("Average waiting time: %.2f\n", (float)totalWaitingTime / n);
+    printf("Average turnaround time: %.2f\n", (float)totalTurnAroundTime / n);
+}
+
+int main() {
+    Process proc[] = {
+        {1, 6, 0, 0},
+        {2, 8, 0, 0},
+        {3, 7, 0, 0},
+        {4, 3, 0, 0}
+    };
+    int n = sizeof(proc) / sizeof(proc[0]);
+
+    findWaitingTime(proc, n);
+    findTurnAroundTime(proc, n);
+    findAvgTime(proc, n);
+
+    return 0;
+}
+
